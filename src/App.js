33de48e70c351f25195147c12f3adafb46220a90
Tanisha Gupta <tanishagupta27200@gmail.com>
2023-04-08 12:15:19 +0530

    Homepage and Contactus

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Homepage from './components/Homepage';
+import Contactus from './components/Contactus';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+    <Header />
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Homepage/>}></Route>
+      <Route path="/contact" element={<Contactus/>}></Route>
+    </Routes>
+    </Router>
+  </>
   );
-}
+} 
 
 export default App;
